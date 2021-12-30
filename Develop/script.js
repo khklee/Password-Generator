2e5f0dfaf8017ed510d84897649884bd5c5ddfcc
Kelly Lee <amorfati38@gmail.com>
2021-12-29 23:32:05 -0700

    last polish

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,6 @@
 // Assignment code here
 
-// variable declaration
+// Variable declaration
 const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
 const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const numbers = '1234567890';
@@ -14,55 +14,57 @@ var confirmSpeicial;
 
 var generatePassword = function() {
 
-  // ask for length of the password
+  // Ask for length of the password
   var lengthprompt = prompt('How long your password would be? Minimum 8 to maximum 128 characters.');
 
-    // validate prompt answer
+    // Validate prompt answer
     if (lengthprompt < 8 || lengthprompt > 128) {
       alert('You need to provide a valid answer! Please try again.');
       return generatePassword();
     }
-      // log a response
+    // Log a response
     console.log('The password length will be ' + lengthprompt + '.');
     
     
-  // continues to ask for character types
+  // Ask for character types
   askCharTypes();
 
-    // validate confirm answers
+    // Validate confirm answers
     if (!confirmLowercase && !confirmUppercase && !confirmNumeric && !confirmSpeicial) {
       alert('You need to choose at least one character type! Please try again.');
       return askCharTypes();
     }
   
-  // if one character type was included
-    if (confirmLowercase) {
+  // See what character types were included
+  if (confirmLowercase) {
      charTypes += lowerCase;
-    }
-  
+  }
+  // Log a response
+  console.log('The password will include ' + lowerCase + '.');
+    
   if (confirmUppercase) {
      charTypes += upperCase;
-    }
-
+  }
+  // Log a response
+  console.log('The password will include ' + upperCase + '.');
+    
   if (confirmNumeric) {
      charTypes += numbers;
   }
-
+  // Log a response
+  console.log('The password will include ' + numbers + '.');
+    
   if (confirmSpeicial) {
      charTypes += specialChar;
   }
+  // Log a response
+  console.log('The password will include ' + specialChar + '.');
 
+  // Select random characters
   for (var i = 0; i < lengthprompt; i++) {
     password  += charTypes.charAt(Math.floor(Math.random() * charTypes.length));
   }
   return password;
-};
-
-function askCharTypes () {
-  confirmLowercase = confirm('Click OK to confirm if you would like to include lowercase characters.');
-  confirmUppercase = confirm('Click OK to confirm if you would like to include uppercase characters.');
-  confirmNumeric = confirm('Click OK to confirm if you would like to include numeric characters.');
-  confirmSpeicial = confirm('Click OK to confirm if you would like to include special characters.');
 };
 
 // Get references to the #generate element
@@ -80,3 +82,11 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Function to ask character types
+function askCharTypes () {
+  confirmLowercase = confirm('Click OK to confirm if you would like to include lowercase characters.');
+  confirmUppercase = confirm('Click OK to confirm if you would like to include uppercase characters.');
+  confirmNumeric = confirm('Click OK to confirm if you would like to include numeric characters.');
+  confirmSpeicial = confirm('Click OK to confirm if you would like to include special characters.');
+};
