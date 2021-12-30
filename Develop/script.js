18f0339cc72f6299dc2f4b6832489b05e34c3251
Kelly Lee <amorfati38@gmail.com>
2021-12-29 20:24:22 -0700

    change character types

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,13 @@
 // Assignment code here
+
+// variable arrays
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "=", "+"];
+
 var generatePassword = function() {
+
   // ask for length of the password
   var lengthprompt = window.prompt("How long your password would be? Minimum 8 to maximum 128 characters.");
 
@@ -8,12 +16,27 @@ var generatePassword = function() {
       window.alert("You need to provide a valid answer! Please try again.");
       return generatePassword();
     }
-
-    // log a response
+      // log a response
     console.log("The password length will be " + lengthprompt + ".");
+    
+    
+  // continues to ask for character types
+    var confirmLowercase = window.confirm("Click OK to confirm if you would like to include lowercase characters.");
+    var confirmUppercase = window.confirm("Click OK to confirm if you would like to include uppercase characters.");
+    var confirmNumeric = window.confirm("Click OK to confirm if you would like to include numeric characters.");
+    var confirmSpeicial = window.confirm("Click OK to confirm if you would like to include special characters.");
+       
+      // validate confirm answers
+      if (confirmLowercase === false && confirmUppercase === false && confirmNumeric === false && confirmSpeicial === false) {
+        window.alert("You need to choose at least one character type! Please try again.");
+        return generatePassword();
+      }
+  
+
+    
 
   // ask for character types to include lowercase only, uppercase only or mixed?
-  var caseOptionPrompt = window.prompt(
+  /* var caseOptionPrompt = window.prompt(
     "Would you like to include LOWERCASE only, UPPERCASE only, or MIXED together? Please enter one 1 for LOWERCASE, 2 for UPPERCASE, or 3 for MIXED."
     );
         
@@ -82,13 +105,18 @@ var generatePassword = function() {
       } else {
         // log a response
         console.log("The password will not include numbers" + password.special + ".");
-      }
+      } 
 
+      // generate random password
+
+      for (var i = 0; i = lengthprompt; i++) {
+        randomPassword  += keyCharacters.charAt(Math.floor(Math.random() * lengthprompt));
+        return password;
+      
+      } */
 };
 
-
-
-  // Get references to the #generate element
+// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
  
 
@@ -105,11 +133,3 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-var password = { 
-  lowercase: "abcdefghijklmnopqrstuvwxyz", 
-  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  mixed: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  numbers: "1234567890",
-  special: "~!@#$%^&*()_+-=`{}:;,.<>?/"
-
-};
